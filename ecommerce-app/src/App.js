966c12686539903719d6c755ad20fcdf17bd86be
Eric C <67345755+Eric-Chien-1@users.noreply.github.com>
2023-04-29 16:03:22 -0500

    clean error

--- ecommerce-app/src/App.js
+++ ecommerce-app/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Navbar from './components/Navigation';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Carousel from './components/Carousel';
 
 
 function App() {
   return (<>
     <Navbar />
+    <Carousel />
     <footer>
       <div className='footer-container'>
         <p>Developed by: Eric Chien and Jaylon Garza</p>
