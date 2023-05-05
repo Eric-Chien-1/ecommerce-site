e73a0734d7c4a22209d4b3e6ee6993b2ad919d0d
Eric C <67345755+Eric-Chien-1@users.noreply.github.com>
2023-05-04 20:02:52 -0500

    front-end stuff

--- ecommerce-app/src/components/Home.js
+++ ecommerce-app/src/components/Home.js
@@ -1,9 +1,20 @@
+<<<<<<< Updated upstream
 import ItemCarousel from "./ItemCarousel";
 const Home = () => {
     return (
         <>
             <h1>Welcome User!</h1>
             <ItemCarousel />
+=======
+import Navigation from "./Navigation";
+const Home = () => {
+    return (
+        <>
+            <div className="app-home">
+                <Navigation />
+            </div>
+
+>>>>>>> Stashed changes
         </>
     )
 }
